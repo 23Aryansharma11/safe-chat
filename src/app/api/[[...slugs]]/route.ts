0fc233a730d1d95c983497eb251eb4511b67b3bd
23Aryansharma11 <23aryansharma11@gmail.com>
2025-12-16 01:24:14 +0530

    update cors

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,9 +1,18 @@
+import { cors } from "@elysiajs/cors";
 import { Elysia } from "elysia";
 // apis
 import { messages } from "@/app/api/messages";
 import { room } from "@/app/api/room";
 
-const app = new Elysia({ prefix: "/api" }).use(room).use(messages);
+const app = new Elysia({ prefix: "/api" })
+  .use(
+    cors({
+      origin: ["https://safe-chat-tan.vercel.app/"],
+      credentials: true,
+    }),
+  )
+  .use(room)
+  .use(messages);
 
 export const GET = app.fetch;
 export const POST = app.fetch;
